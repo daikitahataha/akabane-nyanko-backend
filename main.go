f1f6f8ef08110aed236d4fd31d403a8358d37696
daikitahataha <tahara.daiki0914@gmail.com>
2022-01-25 01:31:09 +0900

    gormでのDBとの接続設定を追加

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"akabane_nyanko/backend/src/db"
 	"akabane_nyanko/backend/src/server"
 )
 
 func main() {
 	router := server.GetRouter()
+
+	db.Init()
 	router.Run()
+	db.Close()
 }
